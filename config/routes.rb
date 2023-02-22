4e7e6af03f19960d2a2be55e73114b6cbb75081d
Sofia Magalhaes Clara <sofiamfclara@gmail.com>
2023-02-22 16:35:29 +0000

    restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:new, :create, :show, :index] do
+    resources :reviews, only: [:new, :create, :show, :index]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+@categories = ["Chinese", "Italian", "Japanese", "French", "Belgian"]
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+15.times do
+  restaurant = Restaurant.create!({ name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone_with_country_code, category: @categories.sample })
+  5.times do
+    Review.create!({restaurant: restaurant, rating: rand(0..5), content: Faker::Restaurant.review})
+  end
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
